0b5866cdccfd12d65b78a33b2caacff02d8d69e6
urbaniec remi <urbaniec.remi@gmail.fr>
2020-01-08 19:16:48 +0100

    Debut Parti API du Projet Libvirt

--- src/AppBundle/Controller/GestionVmController.php
+++ src/AppBundle/Controller/GestionVmController.php
@@ -32,47 +32,7 @@ class GestionVmController extends Controller
      */
     public function homeAction(Request $request)
     {
-        if(isset ($_SESSION["role"])){
-            $user = $_SESSION["user"];
-            $userRole = $_SESSION["role"];
-            if ($userRole == "Admin" or $userRole == "User" ){
-                $repository = $this->getDoctrine()->getRepository(Vm::class);
-                $vms = $repository->findBy(
-                    ['user' => $user],
-                    ['name' => 'ASC']
-                );
-                dump($vms);
-                
-                dump($user);
-                dump($userRole);
-                $repository = $this->getDoctrine()->getRepository(Pool::class);
-                $pools = $repository->findBy(
-                    ['user' => $user],
-                    ['name' => 'ASC']
-                );
-                $repository = $this->getDoctrine()->getRepository(Vm::class);
-                //$vms = $repository->findByUser($user);
-                //$vms2 = $repository->findAll();
-                //array('user' => $user,'pool' => null ),
-                dump($vms);
-                //dump($vms2);
-                dump($pools);
-                
-                return $this->render('gestionVm/homePage.html.twig', [
-                    'user' => $user,
-                    'role' => $userRole,
-                    'vms' => $vms,
-                    'pools'=>$pools,
-                ]);
-            }else{
-                return $this->redirectToRoute('login');
-            }
-
-
-        }else{
-            return $this->redirectToRoute('login');
-        }
-        //return $this->render('connexion/login.html.twig');
+        return $this->render('default/index.html.twig');
     }
 
 }
